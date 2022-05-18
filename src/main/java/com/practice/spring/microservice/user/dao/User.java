1fa10dc3c2a365dc357bada261b938b20cc96c35
Srijan-18 <srijan.mishrasir@gmail.com>
2022-05-18 22:58:34 +0530

    Modified:  use of lombok for getters and setters

--- src/main/java/com/practice/spring/microservice/user/dao/User.java
+++ src/main/java/com/practice/spring/microservice/user/dao/User.java
@@ -1,9 +1,19 @@
 package com.practice.spring.microservice.user.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ToString
+@AllArgsConstructor
+@Setter
+@Getter
 public class User {
 
     private Integer id;
@@ -13,43 +23,4 @@ public class User {
 
     @Past(message = "Birth Date can not be greater than present")
     private Date birthDate;
-
-    public User(Integer id, String name, Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
 }
