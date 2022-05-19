5d2aab4fc890ad3ccb18caccea2bc6698113379e
Srijan-18 <srijan.mishrasir@gmail.com>
2022-05-19 22:59:04 +0530

    Added: Static filtering of field((Ignoring a field that should not be a part of response).

--- src/main/java/com/practice/spring/microservice/user/dao/User.java
+++ src/main/java/com/practice/spring/microservice/user/dao/User.java
@@ -1,5 +1,6 @@
 package com.practice.spring.microservice.user.dao;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class User {
     @Size(min = 3, message = "Name must have at least 3 characters")
     private String name;
 
+    @JsonIgnore //Ignore this field in all responses.
     @Past(message = "Birth Date can not be greater than present")
     private Date birthDate;
 }
